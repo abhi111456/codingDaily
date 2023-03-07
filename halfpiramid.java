13da655eff2efeddf5435160341fbf3698b38c78
abhi111456 <abhishek.21jics039@jietjodhpur.ac.in>
2023-03-08 01:08:09 +0530

    pattern

---
+++ halfpiramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class halfpiramid {
+    public static void main(String args[]) {
+        int n = 4;
+        int m = 5;
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
